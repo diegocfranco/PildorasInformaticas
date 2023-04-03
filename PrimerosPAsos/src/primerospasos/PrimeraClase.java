56d45b78c94adf3c25abc8024c5d913ae967d966
Juan Diego <juan.diego.castano@correounivalle.edu.co>
2023-04-03 16:07:36 -0500

    Proyecto inicial

---
+++ PrimerosPAsos/src/primerospasos/PrimeraClase.java
@@ -0,0 +1,16 @@
+package primerospasos;
+
+/**
+ *
+ * @author kali
+ */
+public class PrimeraClase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("prmer Programa");
+    }
+    
+}
