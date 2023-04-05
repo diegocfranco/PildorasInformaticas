96daeee30434d43b6ffa33effb7c92307431a87d
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-05 15:49:41 -0500

    Modicicacion de la clase CalculosConMath utilizando pow y refundicion(int)

--- PrimerosPAsos/src/primerospasos/CalculosConMath.java
+++ PrimerosPAsos/src/primerospasos/CalculosConMath.java
@@ -7,9 +7,20 @@ package primerospasos;
  */
 public class CalculosConMath {
     public static void main(String[] args) {
-      //int raiz =Math.sqrt(9.0); no se puede ya que es double y almacena en int
+    /** int raiz =Math.sqrt(9.0); no se puede ya que es double y almacena en int
         double raiz =Math.sqrt(9.0);
         System.out.println("la raiz es "+ raiz);
+    
+    double num1 = 3.43F;
+    int resultado = (int)Math.round(num1);
+    System.out.println(resultado);
+    **/
+    double base = 5;
+    double exponente = 3;
+    int resultado = (int)Math.pow(base,exponente);
+    System.out.println("El resultado de "+base+" elevado a la "+exponente+" es "+ resultado);
+    
+    
     }
     
 }
