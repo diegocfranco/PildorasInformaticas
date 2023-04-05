d38bb1ca40477ab802346c3fe481e306a4fa3864
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-05 14:31:58 -0500

    se añadio la clase DeclaracionesOperadores

---
+++ PrimerosPAsos/src/primerospasos/DeclaracionesOperadores.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package primerospasos;
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class DeclaracionesOperadores {
+    
+    public static void main(String[] args) {
+        double a =5;
+        double b;
+        b = 10;
+        double c = b / a;
+        //c += 6; 
+        System.out.println(c);
+    
+    }
+    
+}
--- PrimerosPAsos/src/primerospasos/PrimeraClase.java
+++ PrimerosPAsos/src/primerospasos/PrimeraClase.java
@@ -6,9 +6,6 @@ package primerospasos;
  */
 public class PrimeraClase {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hola Alumnos");
     }
