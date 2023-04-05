8df335bc9198393519acf490c5aadd3c923be33c
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-05 14:42:43 -0500

    añadido pdf ejericio_1 y clase ejercicio 1 con correspondiente solucion

---
+++ PrimerosPAsos/src/primerospasos/Ejercicio_1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package primerospasos;
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class Ejercicio_1 {
+    public static void main(String[] args) {
+        String  nombre = "Ana";
+        short edad = 27;
+        double salario = 1800.50;
+        Boolean carnet = true;
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+        System.out.println(carnet);
+        
+    }
+    
+}
