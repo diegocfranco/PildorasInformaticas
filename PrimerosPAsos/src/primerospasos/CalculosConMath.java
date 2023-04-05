7cf8d86d9c8b60fbb3ef3b0180a919589ce9d6bf
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-05 15:34:05 -0500

    Añadida la clase CalculosConMath

---
+++ PrimerosPAsos/src/primerospasos/CalculosConMath.java
@@ -0,0 +1,15 @@
+
+package primerospasos;
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class CalculosConMath {
+    public static void main(String[] args) {
+      //int raiz =Math.sqrt(9.0); no se puede ya que es double y almacena en int
+        double raiz =Math.sqrt(9.0);
+        System.out.println("la raiz es "+ raiz);
+    }
+    
+}
