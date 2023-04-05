1094a27e6a852378ae03394bc6cbe495d1e3cdd8
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-05 15:11:24 -0500

    modificacion de la clase Declaraciones operadores y adicion de ejercicio 2 y su solucion

--- PrimerosPAsos/src/primerospasos/DeclaracionesOperadores.java
+++ PrimerosPAsos/src/primerospasos/DeclaracionesOperadores.java
@@ -11,13 +11,29 @@ package primerospasos;
 public class DeclaracionesOperadores {
     
     public static void main(String[] args) {
-        double a =5;
+     /**double a =5;
         double b;
         b = 10;
         double c = b / a;
         //c += 6; 
         System.out.println(c);
-    
+        
+        
+        final int c = 6;
+        c=8 -> no se puede porque es final
+        
+        final double apulgada = 2.54;
+        double cm = 6;
+        double resultado = cm/apulgada;
+        
+        System.out.println("En "+ cm +" centimetros hay "+resultado+" pulgadas");
+        **/
+        int operador1,operador2,resultado;
+        operador1=8;
+        operador2=7;
+        resultado=operador1+operador2;
+        System.out.println("El resultado es "+resultado);
+        
     }
     
 }
---
+++ PrimerosPAsos/src/primerospasos/Ejercicio_2.java
@@ -0,0 +1,16 @@
+package primerospasos;
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class Ejercicio_2 {
+    public static void main(String[] args) {
+        final double dolar = 1.137;
+        final double salario =2125.70;
+        double salarioEnDolar;
+        String nombre = "Manuel";
+        salarioEnDolar=dolar*salario;
+        System.out.println("El salario de "+nombre+"en dolares es "+salarioEnDolar);
+    }
+}
