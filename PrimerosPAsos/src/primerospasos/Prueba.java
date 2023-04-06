60e4cab63e8ad5b49cc687c38015ed22770d620f
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-06 13:43:17 -0500

    añadida clase prueba para explicacion de paquetes de Java

---
+++ PrimerosPAsos/src/primerospasos/Prueba.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+package primerospasos;
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class Prueba {
+    public static void main(String[] args){
+        String nombre;
+        Scanner miObjeto1;
+        Locale miObjeto2;
+        
+    }
+    
+}
