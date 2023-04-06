9513a2f0245ac6b876b780ccb2ff4f43344bcb3a
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-06 13:27:59 -0500

    se añadio la clase ManipulaCadenasIII usando equals() y equalsIgnoreCase()

---
+++ PrimerosPAsos/src/primerospasos/ManipulaCadenasIII.java
@@ -0,0 +1,16 @@
+
+package primerospasos;
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class ManipulaCadenasIII {
+    public static void main(String[] args){
+        String alumnoI = "David";
+        String alumnoII = "david";
+        System.out.println(alumnoI.equals(alumnoII));
+        System.out.println(alumnoI.equalsIgnoreCase(alumnoII));
+    }
+    
+}
