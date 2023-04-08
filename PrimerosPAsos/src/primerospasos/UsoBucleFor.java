7c1fe9d06a676b069af93256c5a6c3a1545ec63f
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-08 10:44:22 -0500

    añadida clase UsoBucleFor

---
+++ PrimerosPAsos/src/primerospasos/UsoBucleFor.java
@@ -0,0 +1,19 @@
+package primerospasos;
+
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class UsoBucleFor {
+    public static void  main(String[]args){
+        
+        for(int i = 10;i!=0;i--){
+            
+            System.out.println("Juaaan ,juanito juan");
+            
+        }
+        
+    }
+    
+}
