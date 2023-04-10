233d4295d383fd914d0d887d80f1c03fd2589538
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-10 09:47:34 -0500

    añadida la clase UsoArrays

---
+++ PrimerosPAsos/src/primerospasos/UsoArray.java
@@ -0,0 +1,33 @@
+package primerospasos;
+
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class UsoArray {
+    public static void main(String[] args){
+        /**
+        int [] miMatriz = new int[5];
+        miMatriz [0] =78;
+        miMatriz [1] =58;
+        miMatriz [2] =87;
+        miMatriz [3] =98;
+        miMatriz [4] =88;
+       
+        System.out.println(miMatriz[0]);
+        System.out.println(miMatriz[1]);
+        System.out.println(miMatriz[2]);
+        System.out.println(miMatriz[3]);
+        System.out.println(miMatriz[4]);**/
+         int[] miMatriz = {2,58,54,5,98,56,81,5,58,2,4,369,5};
+        
+        for(int i =0;i< miMatriz.length;i++){
+        
+        System.out.println("Valor del indice " + i + " es "+miMatriz[i]);
+        
+    }
+        
+    }
+    
+}
