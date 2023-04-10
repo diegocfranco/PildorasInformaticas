4c8d5d9b0634a3ae761d41d9d243cd7a64d7d36e
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-10 17:58:38 -0500

    añadido paquete poo con clase Coche y UsoCoche

---
+++ PrimerosPAsos/src/poo/Coche.java
@@ -0,0 +1,22 @@
+package poo;
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class Coche {
+    int ruedas;
+    int ancho;
+    int largo;
+    int motor;
+    int peso;
+    
+    public Coche(){
+        int ruedas=4;
+        int ancho=2000;
+        int largo=300;
+        int motor=1600;
+        int peso=500;  
+    }
+    
+}
---
+++ PrimerosPAsos/src/poo/UsoCoche.java
@@ -0,0 +1,15 @@
+package poo;
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class UsoCoche {
+    
+    public static void main (String[] args){
+        
+        Coche Renault = new Coche();
+        System.out.println("este coche tiene " + Renault.ruedas + " ruedas");
+        
+    }
+    
+}
