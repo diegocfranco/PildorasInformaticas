b8dfd3f0b4f62f5b58a936ee375364d654f15f5f
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-10 20:38:16 -0500

    actualizacion de las clases Coche y UsoCoche con metodos getter y setter

--- PrimerosPAsos/src/poo/Coche.java
+++ PrimerosPAsos/src/poo/Coche.java
@@ -5,18 +5,117 @@ package poo;
  * juan.diego.castano@correounivalle.edu.co
  */
 public class Coche {
-    int ruedas;
-    int ancho;
-    int largo;
-    int motor;
-    int peso;
+
+    private int ruedas;
+    private int ancho;
+    private int largo;
+    private int motor;
+    private int pesoPlataforma;
+    private String color;
+    private int pesoTotal;
+    private boolean AsientosCuero,aireAcondicionado;
+    
+    
+    public String getInfoCoche(){
+        
+        return "la plataforma del vehiculo tiene "+ruedas+" ruedas, mide "+
+                (largo/1000)+" metros y un ancho de "+ancho+"  cm de ancho";
+    }
+    
+    public int getRuedas() {
+        return ruedas;
+    }
+
+    public void setRuedas(int ruedas) {
+        this.ruedas = ruedas;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+    public int getMotor() {
+        return motor;
+    }
+
+    public void setMotor(int motor) {
+        this.motor = motor;
+    }
+
+    public int getPeso() {
+        return pesoPlataforma;
+    }
+
+    public void setPeso(int peso) {
+        this.pesoPlataforma = peso;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor() {
+        color = "azul";
+    }
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public String dimePesoTotal(){
+        int pesoCarroceria=500;
+        pesoTotal=pesoCarroceria+pesoPlataforma;
+        if(AsientosCuero==true){pesoTotal+=50;}
+        if(aireAcondicionado==true){pesoTotal+=20;}
+        
+        return "el peso total del coche es: "+pesoTotal;
+        
+    }
+    
+        public String dimePreciFinal(){
+        int precioFinal=10000;
+        if(AsientosCuero==true){precioFinal+=2000;}
+        if(aireAcondicionado==true){precioFinal+=1500;}
+        
+        return "el precio total del coche es: "+precioFinal;
+        
+    }
+
+    public boolean isAsientosCuero() {
+        return AsientosCuero;
+    }
+
+    public void setAsientosCuero(boolean AsientosCuero) {
+        this.AsientosCuero = AsientosCuero;
+    }
+
+    public boolean isAireAcondicionado() {
+        return aireAcondicionado;
+    }
+
+    public void setAireAcondicionado(boolean aireAcondicionado) {
+        this.aireAcondicionado = aireAcondicionado;
+    }
+
     
     public Coche(){
-        int ruedas=4;
-        int ancho=2000;
-        int largo=300;
-        int motor=1600;
-        int peso=500;  
+        ruedas = 4;
+        ancho = 300;
+        largo = 2000;
+        motor = 1600;
+        pesoPlataforma = 500;  
     }
     
 }
--- PrimerosPAsos/src/poo/UsoCoche.java
+++ PrimerosPAsos/src/poo/UsoCoche.java
@@ -1,4 +1,5 @@
 package poo;
+import javax.swing.JOptionPane;
 /**
  * @author Juan Diego Castaño
  * juan.diego.castano@correounivalle.edu.co
@@ -8,7 +9,18 @@ public class UsoCoche {
     public static void main (String[] args){
         
         Coche Renault = new Coche();
-        System.out.println("este coche tiene " + Renault.ruedas + " ruedas");
+        System.out.println("este coche tiene " + Renault.getRuedas() + " ruedas");
+        Renault.setColor(JOptionPane.showInputDialog("introduce un color"));
+        System.out.println("mi coche tiene color " + Renault.getColor());
+        Renault.setColor(JOptionPane.showInputDialog("introduce el nuevo color"));
+        System.out.println("mi coche ahora tiene color " + Renault.getColor());
+        System.out.println(Renault.getInfoCoche());
+        Renault.setAsientosCuero(true);
+        Renault.setAireAcondicionado(true);
+        System.out.println(Renault.dimePesoTotal());
+        System.out.println(Renault.dimePreciFinal());
+        
+       
         
     }
     
