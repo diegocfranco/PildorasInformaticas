7af5e24d280c7b3950a4b51d0ebc4efbb326a0e0
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-10 21:17:29 -0500

    adicionada clase UsoEmpleado

---
+++ PrimerosPAsos/src/poo/UsoEmpleado.java
@@ -0,0 +1,26 @@
+package poo;
+
+import java.util.Date;
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class UsoEmpleado {
+    
+    public static void main(String[] args){
+        
+        
+    }
+    
+}
+class Empleado{
+
+    public Empleado(String nom,double sue,int agno,int mes,int dia){
+        
+  
+    }
+        private String nombre;
+        private double sueldo;
+        private Date altaContrato;
+}
