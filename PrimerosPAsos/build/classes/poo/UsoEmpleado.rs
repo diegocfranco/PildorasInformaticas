935301535839e41408e0c73b33b2cb6a5ed97a1b
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-04-11 10:25:33 -0500

    añadida clase prueba con uso de final

---
+++ PrimerosPAsos/build/classes/poo/UsoEmpleado.rs
@@ -0,0 +1,2 @@
+poo.Empleado
+poo.UsoEmpleado
---
+++ PrimerosPAsos/src/poo/Prueba.java
@@ -0,0 +1,49 @@
+package poo;
+
+/**
+ * @author Juan Diego Castaño
+ * juan.diego.castano@correounivalle.edu.co
+ */
+public class Prueba {
+     public static void main(String[] args){
+         Empleado empleado1= new Empleado("Ana");
+         Empleado empleado2= new Empleado("Paco");
+         empleado2.setSeccion("RRHH");
+         //empleado2.setNombre("paca");
+         System.out.println(empleado1.devuelveDatos());
+         System.out.println(empleado2.devuelveDatos());
+     }
+    
+         
+}
+class Empleado{
+    private final String  nombre;
+    private String seccion;
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getSeccion() {
+        return seccion;
+    }
+
+    public void setSeccion(String seccion) {
+        this.seccion = seccion;
+    }
+
+    
+    public String devuelveDatos(){
+        
+        return "el nombre es "+nombre+" y la seccion es "+seccion;
+    }
+    public Empleado(String nom){
+        nombre=nom;
+        seccion="Administracion";
+        
+    }
+    
+    
+    
+}
